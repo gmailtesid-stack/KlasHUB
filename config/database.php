3cdd74478c1cb0c3bb03b1fd6293e6e5ffc1aeac
readmialhitam-commits <readmialhitam@gmail.com>
2026-05-17 19:50:58 +0700

    Fetch complete Mozilla CA bundle to handle OpenSSL 3.0 intermediate cert validation on Vercel

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                1009 => env('VERCEL') ? '/tmp/isrgrootx1.pem' : (env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : null),
+                1009 => env('VERCEL') ? '/tmp/cacert.pem' : (env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : null),
             ]) + [
                 1014 => false, // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT
             ] : [],
