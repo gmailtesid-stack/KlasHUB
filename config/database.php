1f909ec34a0555c05e4d18f55f5e64167a6650e9
readmialhitam-commits <readmialhitam@gmail.com>
2026-05-17 19:04:03 +0700

    Fix PHP PDO deprecation crash by using hardcoded integer constant

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : env('MYSQL_ATTR_SSL_CA'),
+                1009 => env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
