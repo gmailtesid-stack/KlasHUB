a3a872f5a08c21d7ca29ea8ef940182abd8ac874
readmialhitam-commits <readmialhitam@gmail.com>
2026-05-21 13:25:25 +0700

    fix: force https scheme for production and vercel environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production' || strpos(config('app.url'), 'https') !== false) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
