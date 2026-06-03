93b68c9a49f24cc0a74b6f485bbac526c14a1e43
readmialhitam-commits <readmialhitam@gmail.com>
2026-06-03 22:01:54 +0700

    chore(seed): update official class roster in admin seeder

--- database/seeders/KelasHubAdminSeeder.php
+++ database/seeders/KelasHubAdminSeeder.php
@@ -25,10 +25,43 @@ class KelasHubAdminSeeder extends Seeder
 
         // 2. Seed Students for Class 06TPLE013
         $students13 = [
+            // Pengurus Inti & Super Admin
             ['nim' => '231011403268', 'name' => 'ARIYAS PRATAMA RAMADHAN', 'role' => 'super_admin', 'pw' => '231011403268KK'],
-            ['nim' => '231011403269', 'name' => 'SEKRETARIS KELAS', 'role' => 'sekretaris', 'pw' => '231011403269SK'],
-            ['nim' => '231011403270', 'name' => 'BENDAHARA KELAS', 'role' => 'bendahara', 'pw' => '231011403270BD'],
-            ['nim' => '231011403271', 'name' => 'MAHASISWA BIASA', 'role' => 'mahasiswa', 'pw' => '231011403271'],
+            ['nim' => '231011402314', 'name' => 'DORRA LADY AFISHE', 'role' => 'bendahara', 'pw' => '231011402314BD'],
+            ['nim' => '231011401386', 'name' => 'MOH FADEL FARISTA', 'role' => 'sekretaris', 'pw' => '231011401386SK'],
+
+            // Mahasiswa Reguler
+            ['nim' => '231011402802', 'name' => 'ARFIANNISA KAYLA', 'role' => 'mahasiswa', 'pw' => '231011402802'],
+            ['nim' => '231011402460', 'name' => 'ARYA HAIDAR SARIFUDIN', 'role' => 'mahasiswa', 'pw' => '231011402460'],
+            ['nim' => '231011402845', 'name' => 'AZAY AGUSTIAN', 'role' => 'mahasiswa', 'pw' => '231011402845'],
+            ['nim' => '231011400535', 'name' => 'BIMO MUSTHAFA ABDILLAH', 'role' => 'mahasiswa', 'pw' => '231011400535'],
+            ['nim' => '231011403640', 'name' => 'DEDE SANDI', 'role' => 'mahasiswa', 'pw' => '231011403640'],
+            ['nim' => '231011402466', 'name' => 'DWI FITRIA ALFIANI', 'role' => 'mahasiswa', 'pw' => '231011402466'],
+            ['nim' => '231011402430', 'name' => 'FACHRY RAMADHAN', 'role' => 'mahasiswa', 'pw' => '231011402430'],
+            ['nim' => '231011402157', 'name' => 'FARIQ ITSBAT ALFARIDZI', 'role' => 'mahasiswa', 'pw' => '231011402157'],
+            ['nim' => '231011401383', 'name' => 'HANA RIFDAH RIANRA', 'role' => 'mahasiswa', 'pw' => '231011401383'],
+            ['nim' => '231011401982', 'name' => 'IHSAN MAULANA', 'role' => 'mahasiswa', 'pw' => '231011401982'],
+            ['nim' => '231011403196', 'name' => 'JUAN MONTOYA DARMAWAN', 'role' => 'mahasiswa', 'pw' => '231011403196'],
+            ['nim' => '231011400980', 'name' => 'MOCHAMAD FICKRY SATRIA', 'role' => 'mahasiswa', 'pw' => '231011400980'],
+            ['nim' => '231011401602', 'name' => 'MUHAMAD ARIO ISNANDAR', 'role' => 'mahasiswa', 'pw' => '231011401602'],
+            ['nim' => '231011401601', 'name' => 'MUHAMAD KHAERULLAH', 'role' => 'mahasiswa', 'pw' => '231011401601'],
+            ['nim' => '231011403096', 'name' => 'MUHAMAD LUTFI AZIZAN', 'role' => 'mahasiswa', 'pw' => '231011403096'],
+            ['nim' => '231011402859', 'name' => 'MUHAMMAD BIMO TRI NUGROHO', 'role' => 'mahasiswa', 'pw' => '231011402859'],
+            ['nim' => '231011400772', 'name' => 'MUHAMMAD FAHMI', 'role' => 'mahasiswa', 'pw' => '231011400772'],
+            ['nim' => '231011400815', 'name' => 'MUHAMMAD ILHAM', 'role' => 'mahasiswa', 'pw' => '231011400815'],
+            ['nim' => '231011403288', 'name' => 'MUHAMMAD NAJHAN TSAANI', 'role' => 'mahasiswa', 'pw' => '231011403288'],
+            ['nim' => '231011400775', 'name' => 'NAJWA MASAYU AZZAHRA', 'role' => 'mahasiswa', 'pw' => '231011400775'],
+            ['nim' => '231011402280', 'name' => 'OLIVIA RAMADHANI', 'role' => 'mahasiswa', 'pw' => '231011402280'],
+            ['nim' => '231011400795', 'name' => 'PASCAL ISMAIL', 'role' => 'mahasiswa', 'pw' => '231011400795'],
+            ['nim' => '231011401598', 'name' => 'PUTRI AMALIA', 'role' => 'mahasiswa', 'pw' => '231011401598'],
+            ['nim' => '231011401384', 'name' => 'RAFI MUHAMMAD LUTHFI', 'role' => 'mahasiswa', 'pw' => '231011401384'],
+            ['nim' => '231011400785', 'name' => 'RAGIL FADHILAH AKHDAN', 'role' => 'mahasiswa', 'pw' => '231011400785'],
+            ['nim' => '231011403294', 'name' => 'REVRICO RAMADHINO IRZAN', 'role' => 'mahasiswa', 'pw' => '231011403294'],
+            ['nim' => '231011401590', 'name' => 'RIZKI ARTINIO PERMANA PUTRA', 'role' => 'mahasiswa', 'pw' => '231011401590'],
+            ['nim' => '231011400813', 'name' => 'SEPTIANO ALVIAN ISMAU', 'role' => 'mahasiswa', 'pw' => '231011400813'],
+            ['nim' => '221011402323', 'name' => 'SYAID AGIL AL MUNAWAR', 'role' => 'mahasiswa', 'pw' => '221011402323'],
+            ['nim' => '231011400761', 'name' => 'WISNU SAPUTRA', 'role' => 'mahasiswa', 'pw' => '231011400761'],
+            ['nim' => '231011402258', 'name' => 'YASIN KAMIL', 'role' => 'mahasiswa', 'pw' => '231011402258'],
         ];
 
         foreach ($students13 as $s) {
