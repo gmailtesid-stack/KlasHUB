952c7624ac37e6b54b11ea60878464d96f2c7f80
gmailtesid-stack <gmailtes.id@gmail.com>
2026-06-20 21:29:00 +0700

    fix: Remove BelongsToClass from MasterSubject so attendance shows for all users

--- app/Models/MasterSubject.php
+++ app/Models/MasterSubject.php
@@ -3,11 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\BelongsToClass;
 
 class MasterSubject extends Model
 {
-    use BelongsToClass;
-
     protected $fillable = ['class_id', 'name', 'sks', 'code', 'default_lecturer'];
 }
