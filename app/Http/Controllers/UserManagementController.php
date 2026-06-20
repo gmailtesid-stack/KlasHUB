3f81f870c3c5cb5ee4c5a078740cf9df76b5c0b5
gmailtesid-stack <gmailtes.id@gmail.com>
2026-06-20 13:21:52 +0700

    fix(auth): firmly enforce NIM suffix policy explicitly for admins based on user standard

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -31,6 +31,13 @@ class UserManagementController extends Controller
         }
 
         $password = $data['nim'];
+        if ($data['role'] === 'ketua_kelas' || $data['role'] === 'super_admin')
+            $password .= 'KK';
+        elseif ($data['role'] === 'sekretaris')
+            $password .= 'SK';
+        elseif ($data['role'] === 'bendahara')
+            $password .= 'BD';
+
         $data['password'] = bcrypt($password);
 
         $student = Student::create($data);
@@ -96,6 +103,12 @@ class UserManagementController extends Controller
 
         $role = $request->role;
         $newPassword = $targetStudent->nim;
+        if ($role === 'ketua_kelas' || $role === 'super_admin')
+            $newPassword .= 'KK';
+        elseif ($role === 'sekretaris')
+            $newPassword .= 'SK';
+        elseif ($role === 'bendahara')
+            $newPassword .= 'BD';
 
         $targetStudent->update([
             'role' => $role,
